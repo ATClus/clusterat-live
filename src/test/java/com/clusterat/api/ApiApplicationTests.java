7aee72e009a144a986fb5354d565b8cd803f7d05
Jhonnatthan Alves <jaalvest@outlook.com>
2025-11-10 17:22:16 -0300

    feat(project): inicializar aplicação Spring Boot
    - Configurado projeto base com Spring Boot
    - Adicionados arquivos de configuração inicial, incluindo `application.properties` e `pom.xml`
    - Incluído suporte a WebFlux, Liquibase e PostgreSQL
    - Adicionado Maven Wrapper para gerenciamento de dependências
    - Criada estrutura inicial de diretórios e classes
    
    Signed-off-by: Jhonnatthan Alves <jaalvest@outlook.com>

---
+++ src/test/java/com/clusterat/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clusterat.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

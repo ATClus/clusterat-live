9432edba7ad9000039d4da3160d217a18527e54e
Jhonnatthan Alves <jaalvest@outlook.com>
2025-11-16 07:11:04 -0300

    refactor(controller): ajustar rota base em DocumentsController
    - Alterada rota de `/api/v1/documents` para `/v1/documents`
    
    Signed-off-by: Jhonnatthan Alves <jaalvest@outlook.com>

--- src/main/java/com/clusterat/live/controller/DocumentsController.java
+++ src/main/java/com/clusterat/live/controller/DocumentsController.java
@@ -26,7 +26,7 @@ import java.util.Optional;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/documents")
+@RequestMapping("/v1/documents")
 public class DocumentsController {
     private final DocProcessorService docProcessorService;
     private final FilesService filesService;
